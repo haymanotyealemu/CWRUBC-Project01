48df00d1969357e9d095cd317320b9ef00af29e4
John L Spetz <JohnLSpetz@zoho.com>
2020-07-10 14:46:08 -0400

    Completed basic hookup of standard speech recognition.

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 $(document).ready(function() {
 
   //var GoogleCloudSpeechToTextAPIKey = "AIzaSyCfh3JoXvDKFCwc3Wud0i8kUJdPaXpJJ4s";
-  var GoogleCloudSpeechToTextAPIKey = "AIzaSyDeuhX7sntzkbREuJ2ZTZoeh8JriCXydJc";
+  //var GoogleCloudSpeechToTextAPIKey = "AIzaSyDeuhX7sntzkbREuJ2ZTZoeh8JriCXydJc";
   var GoogleTranslationAPIKey = "AIzaSyBKTUVOk5a2Ud1rXCbkf8YAXDbaPzY5pyA";
 
-  var speechRecorder = window.webkitSpeechRecognition;
-  var recognition = new speechRecognition();
-  //text box
-  var textbox = $("#textbox");
-  //how to use machine - instructions 
-  var content = $("#instructions")
-  //what we talk into - talking into 
+  // Follow MDN advice on making "webkit" prefix optional in anticipation
+  // of a name standardization once this API becomes widely available
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+  var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+  var recognition = new SpeechRecognition();
+
+  function lightningAnimate() {
+    $(".gif").attr("src", $(".gif").attr("data-animate"));
+    $(".gif").attr("data-state", "animate");
+  }
+
+  function lightningStill() {
+    $(".gif").attr("src", $(".gif").attr("data-still"));
+    $(".gif").attr("data-state", "still");
+  }
+
+  var textbox = $("#text-display");
+  // Where rendered text is displayed
   var content = '';
   recognition.continous = true;
-  var instruction = $("#instruction");
+
+  // Show status or instructions
+  //var instruction = $("#status");
 
   recognition.onstart =function () {
-    instruction.text("Voice Recognition is on ");
+    lightningAnimate()
+    // instruction.text("Voice Recognition is on ");
   }
 
   recognition.onspeechend = function () {
-    instruction.text('No Activity');
+    lightningStill()
+    // instruction.text('No Activity');
   }
 
   recognition.onerror = function () {
-    instruction.text("Try Again");
+    // instruction.text("Try Again");
   }
 
   recognition.onresult = function (event) {
@@ -36,14 +52,14 @@ $(document).ready(function() {
 
   $("#start").click(function (event) {
     if (content.length) {
-      content += '';
+      content += '\n';
     }
+    recognition.start();
   });
 
   $("#stop").click(function (event) {
+    lightningStill()
     recognition.stop();
   });
-
-  recognition.start();
 
 });
