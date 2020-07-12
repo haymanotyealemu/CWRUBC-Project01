57ae8ce2ce905ad348ee29f520cac55343e46c33
John L Spetz <JohnLSpetz@zoho.com>
2020-07-12 13:28:53 -0400

    Add passPhraseToMSTranslator() for translation

--- script.js
+++ script.js
@@ -41,6 +41,8 @@ $(document).ready(function() {
         phrasesAsRecorded.push(e.result.text)
         // Display all the snippets starting each one on its own line
         $("#text-display").text(phrasesAsRecorded.join("\n"));
+        // Pass to callback for translation
+        passPhraseToMSTranslator(phrasesAsRecorded.length-1,'de');
       } else {
       }
     }
@@ -64,6 +66,48 @@ $(document).ready(function() {
     // Stop the lightning animation as feedback
     lightningToStill();
   });
+
+  function passPhraseToMSTranslator(phraseIndex, lang) {
+    var callData = '';
+    //URL PATTERN: {{{
+    //https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=es
+    //sample value }}}
+    var queryURL = "https://api.cognitive.microsofttranslator.com/translate?"
+    var queryObj = {
+      'api-version':'3.0',
+      to:'de'
+    };
+    var callObj = {
+      async: true,
+      crossDomain: true,
+      url: '',
+      method: 'POST',
+      headers: {
+        'Ocp-Apim-Subscription-Key': 'a9b0d0e3a840456a8257618e073be609',
+        'Ocp-Apim-Subscription-Region': 'global',
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      data: ''
+    }
+    // Assemble query url and call data
+    if (lang) {
+      queryObj.to = lang;
+    }
+    queryURL += $.param(queryObj);
+    console.log('∞° queryURL="'+queryURL,'"');
+    // Set call url to constructed value
+    callObj.url = queryURL;
+    // Set data property to an object with property Text inside an array
+    callObj.data = "[{'Text':'"+phrasesAsRecorded[phraseIndex]+"'}]";
+    console.log('∞° callObj.data="'+callObj.data,'"');
+    callData = JSON.stringify(callObj);
+    console.log('∞° callData="'+callData,'"');
+    // Make asynchronous API call
+    $.ajax(callData).then(function (response) {
+      console.log(response);
+    });
+  }
 
   function initializeSpeechSDK() {
     speechConfig = SpeechSDK.SpeechConfig.fromSubscription(key1MSUnifiedSpeechAPI, regionCode);
